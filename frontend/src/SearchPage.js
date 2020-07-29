68edb4ffb802a28c0a83170ff4331d1f630d475b
ysho <ysho2@illinois.edu>
2020-07-29 13:44:01 -0500

    Added Search Bar

--- frontend/src/SearchPage.js
+++ frontend/src/SearchPage.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { TextField, Box } from '@material-ui/core';
+import { TextField, Box, Button } from '@material-ui/core';
 
 
 export default function SearchPage() {
 
     return(
-        <Box component="main" width="75%" alignItems="center" justify="center" border = {true}>
-             <TextField id="outlined-search" label="Search field" type="search" variant="outlined" fullWidth/>
+        <Box  width="75%" alignItems = "center" justifyContent = "center" spacing={2}>
+            <Box m = {4}>
+                <TextField id="outlined-search" label="Search field" type="search" variant="outlined" fullWidth spacing={2}/>
+            </Box>
+            <Box>
+                <Button variant="outlined" spacing={2}>Search</Button>
+            </Box>
+             
         </Box>
     );
 }
