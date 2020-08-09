91e4eb23a3d4c93f022f4b8945397eca53388d84
ysho <ysho2@illinois.edu>
2020-08-09 07:47:34 -0500

    removed search page fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
 import SignIn from "./SignIn";
-import SearchPage from './screens/SearchPage';
-import SearchResults from './screens/SearchResults';
-import { Container } from 'semantic-ui-react';
-// import { Router } from "@reach/router"
-import TripSearchResults from './screens/SearchResults';
-import TripSearch from './screens/SearchPage';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <SignIn/>
   );
-}
-
-const styles ={
-  containerPadding: {
-    paddingTop: '2em',
-    paddingBottom: '2em'
-  }
 }
 
 export default App;
